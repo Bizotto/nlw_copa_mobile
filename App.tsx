5ba93a745f8e5b75d854a2d15dce6740a0138995
Bizotto <bizotto.nicolas@gmail.com>
2022-11-01 10:53:54 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { THEME } from './src/styles/theme';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { Loading } from './components/Loading';
+import { SignIn } from './screens/SignIn';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
